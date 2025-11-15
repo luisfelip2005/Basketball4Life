e72e5bf3ef9b9cac4813ab9efced928aae265905
luisfelip2005 <gomesluisfelipe09@gmail.com>
2025-11-15 18:19:44 -0300

    adicionando script do banco de dados

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -6,57 +6,33 @@
 comandos para mysql server
 */
 
-CREATE DATABASE aquatech;
+CREATE DATABASE Basketball;
 
-USE aquatech;
+USE Basketball;
 
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+CREATE TABLE User (
+	id_user INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(45) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	height DECIMAL(3, 2),
+	weight DECIMAL(5, 2)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE Training (
+	id_training INT PRIMARY KEY AUTO_INCREMENT,
+	starting_time TIME NOT NULL,
+	ending_time TIME NOT NULL,
+	trainig_date DATE NOT NULL,
+	fk_user INT,
+	FOREIGN KEY (fk_user) REFERENCES User(id_user)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE WeeklyGoal (
+	id_goal INT PRIMARY KEY AUTO_INCREMENT,
+	time_goal INT,
+	qtd_weekly_goal INT,
+	period_of_day VARCHAR(15)
+	fk_user INT,
+	FOREIGN KEY (fk_user) REFERENCES User(id_user)
 );
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);

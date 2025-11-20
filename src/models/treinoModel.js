372a8ff3ae1aacdd42b60ed72ee960df5cc65834
luisfelip2005 <gomesluisfelipe09@gmail.com>
2025-11-20 20:44:03 -0300

    arrumando bug da dashboard da semana

--- src/models/treinoModel.js
+++ src/models/treinoModel.js
@@ -60,7 +60,7 @@ function getConclusionPercent(date, user_id) {
 
 function getAllData(user_id) {
     var instrucaoSql = `
-        SELECT * FROM vw_percent_conclusion WHERE fk_user = '${user_id}' GROUP BY number_of_week;
+        SELECT number_of_week, SUM(total_minutes) AS total_minutes FROM vw_percent_conclusion WHERE fk_user = '${user_id}' GROUP BY number_of_week;
     `
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);

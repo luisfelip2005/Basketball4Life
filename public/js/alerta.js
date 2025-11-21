d0a9a6c20fa4faa74026d735861303ad47bd0598
luisfelip2005 <gomesluisfelipe09@gmail.com>
2025-11-21 19:21:23 -0300

    validando sessão e encaminhando para o login

--- public/js/alerta.js
+++ public/js/alerta.js
@@ -117,9 +117,9 @@ function transformarEmDiv({ idAquario, temp, grauDeAviso, grauDeAvisoCor }) {
     `;
 }
 
-function atualizacaoPeriodica() {
-    JSON.parse(sessionStorage.AQUARIOS).forEach(item => {
-        obterdados(item.id)
-    });
-    setTimeout(atualizacaoPeriodica, 5000);
-}
+// function atualizacaoPeriodica() {
+//     JSON.parse(sessionStorage.AQUARIOS).forEach(item => {
+//         obterdados(item.id)
+//     });
+//     setTimeout(atualizacaoPeriodica, 5000);
+// }

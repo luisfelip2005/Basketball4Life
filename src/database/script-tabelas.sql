903d59fc99442a74f946b006d6b571a4babe7727
luisfelip2005 <gomesluisfelipe09@gmail.com>
2025-11-22 08:49:51 -0300

    alterando tabelas user e goal

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -13,7 +13,7 @@ USE Basketball;
 CREATE TABLE user (
 	id_user INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(45) NOT NULL,
-	email VARCHAR(100) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
 	password VARCHAR(255) NOT NULL,
 	height DECIMAL(3, 2),
 	weight DECIMAL(5, 2)
@@ -29,11 +29,12 @@ CREATE TABLE training (
 );
 
 CREATE TABLE weeklyGoal (
-	id_goal INT PRIMARY KEY AUTO_INCREMENT,
+	id_goal INT AUTO_INCREMENT,
 	time_goal INT,
 	qtd_weekly_goal INT,
 	period_of_day VARCHAR(15),
 	fk_user INT,
+    PRIMARY KEY (id_goal, fk_user),
 	FOREIGN KEY (fk_user) REFERENCES user(id_user)
 );
 
